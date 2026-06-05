eb855dbae0fd9ac3f08c0071d0f13437e80e946e
Richard Lefevre <rlefevre.6505@gmail.com>
2026-06-05 16:10:01 +0100

    removed unneeded font imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,10 @@
-import {
-  Geist,
-  Geist_Mono,
-  Caveat,
-  LXGW_WenKai_Mono_TC,
-} from "next/font/google";
+import { LXGW_WenKai_Mono_TC } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const wenkaiMono = LXGW_WenKai_Mono_TC({
   variable: "--font-wenkai-mono",
   subsets: ["latin"],
   weight: ["400"],
-});
-
-const caveat = Caveat({
-  variable: "--font-caveat",
-  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -35,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${wenkaiMono.variable} ${caveat.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${wenkaiMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
