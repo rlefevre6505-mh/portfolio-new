462f00c36780d89f694cd23244317c6e8f4a854c
Richard Lefevre <rlefevre.6505@gmail.com>
2026-06-08 17:21:23 +0100

    updated email domain

--- src/lib/resend.js
+++ src/lib/resend.js
@@ -9,7 +9,7 @@ export const sendEmail = async (FormData) => {
 
   await resend.emails.send({
     to: process.env.MY_EMAIL,
-    from: `${name} <onboarding@resend.dev>`,
+    from: `${name} <tomtheshopsupport@gmail.com>`,
     subject: `New PORTFOLIO message from ${name}`,
     html: `
       <p><strong>Name:</strong> ${name}</p>
